956503a58aa02336d27b2f2913d01bedf66eea1f
klu2300030925 <163671996+klu2300030925@users.noreply.github.com>
2024-10-18 13:11:38 +0530

    2300030925_Vayilada Venkata Thanuj

---
+++ calc.js
@@ -0,0 +1,14 @@
+function add(a,b) {
+    return a+b;
+}
+function sub(a,b) {
+    return a-b;
+}
+function divide(a,b) {
+    return a/b;
+}
+let a=20;
+let b=10;
+console.log(add(a,b));
+console.log(sub(a,b));
+console.log(divide(a,b));
